817a8537fbcb0a1ce24a98cfd5fac5a3b76d6f81
mgrzybek <mathieu.grzybek@justice.gouv.fr>
2022-04-14 13:01:44 +0000

    [region] add Auth json data

--- internal/core/model_region.go
+++ internal/core/model_region.go
@@ -2,21 +2,14 @@ package core
 
 // Region refers to a k8s cluster
 type Region struct {
-	ID string `json:"id,omitempty"`
-
-	Name string `json:"name,omitempty"`
-
-	Description string `json:"description,omitempty"`
-
-	Location *location `json:"location,omitempty"`
-
-	Services *Services `json:"services,omitempty"`
-
-	OnyxiaAPI *onyxiaAPI `json:"onyxiaAPI,omitempty"`
-
-	Data *data `json:"data,omitempty"`
-
-	Auth *Auth
+	ID          string     `json:"id,omitempty"`
+	Name        string     `json:"name,omitempty"`
+	Description string     `json:"description,omitempty"`
+	Location    *location  `json:"location,omitempty"`
+	Services    *Services  `json:"services,omitempty"`
+	OnyxiaAPI   *onyxiaAPI `json:"onyxiaAPI,omitempty"`
+	Data        *data      `json:"data,omitempty"`
+	Auth        *Auth      `json:"auth,omitempty"`
 }
 
 // Regions designates a list of regions using their name
